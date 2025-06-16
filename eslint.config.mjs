2971307f80ef088c1cc034c882a08bfb79dff510
n3waz <lhk@cesar.school>
2025-06-16 14:28:43 -0300

    chore: eslint config updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -37,6 +37,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-member-access': 'warn',
       '@typescript-eslint/no-unsafe-call': 'warn',
       '@typescript-eslint/no-unsafe-return': 'warn',
+      '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
