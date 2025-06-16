459628f0390988649b84dfe787bc6e3a91ea578b
n3waz <lhk@cesar.school>
2025-06-16 17:04:47 -0300

    fix: rules setted off removed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -37,11 +37,8 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-member-access': 'warn',
       '@typescript-eslint/no-unsafe-call': 'warn',
       '@typescript-eslint/no-unsafe-return': 'warn',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "indent": ["error", 4]
     },
   },
 );
