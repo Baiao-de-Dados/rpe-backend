c746ddb5716aff4c6277f7311b0eee76b52c684c
Marcus Vinicius <mvfranca2004@gmail.com>
2025-06-20 15:32:24 -0300

    feat: restrict evaluations endpoints to RH role only

--- src/pillars/pillars.controller.ts
+++ src/pillars/pillars.controller.ts
@@ -5,6 +5,7 @@ import { CreatePillarDto } from './dto/create-pillar.dto';
 import { UpdatePillarDto } from './dto/update-pillar.dto';
 import { ApiCreate, ApiDelete, ApiGet, ApiUpdate } from 'src/common/decorators/api-crud.decorator';
 import { ApiAuth } from 'src/common/decorators/api-auth.decorator';
+import { OnlyRH } from 'src/auth/decorators/roles.decorator';
 
 @ApiTags('Pilares')
 @ApiAuth()
@@ -12,30 +13,35 @@ import { ApiAuth } from 'src/common/decorators/api-auth.decorator';
 export class PillarsController {
     constructor(private readonly pillarsService: PillarsService) {}
 
+    @OnlyRH()
     @Post()
     @ApiCreate('pilar')
     create(@Body() createPillarDto: CreatePillarDto) {
         return this.pillarsService.create(createPillarDto);
     }
 
+    @OnlyRH()
     @Get()
     @ApiGet('pilares')
     findAll() {
         return this.pillarsService.findAll();
     }
 
+    @OnlyRH()
     @Get(':id')
     @ApiGet('pilar')
     findOne(@Param('id', ParseIntPipe) id: number) {
         return this.pillarsService.findOne(id);
     }
 
+    @OnlyRH()
     @Patch(':id')
     @ApiUpdate('pilar')
     update(@Param('id', ParseIntPipe) id: number, @Body() updatePillarDto: UpdatePillarDto) {
         return this.pillarsService.update(id, updatePillarDto);
     }
 
+    @OnlyRH()
     @Delete(':id')
     @ApiDelete('pilar')
     remove(@Param('id', ParseIntPipe) id: number) {
