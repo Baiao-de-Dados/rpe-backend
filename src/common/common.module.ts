38373878f403cabc835d509cdfc8dac6ffcee2bc
n3waz <lhk@cesar.school>
2025-06-25 17:27:26 -0300

    feat: improved project setups, tag and reference on schema

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SystemConfigService } from './services/system-config.service';
-import { SystemConfigController } from './controllers/system-config.controller';
 
 @Module({
     imports: [PrismaModule],
-    controllers: [SystemConfigController],
     providers: [SystemConfigService],
     exports: [SystemConfigService],
 })
