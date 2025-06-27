4b56f9f54c9c759ddd3081a58a1aa3dda30e4a2a
n3waz <lhk@cesar.school>
2025-06-27 20:15:36 -0300

    feat: improved criteria, pillar and cycle on to rh

--- src/user/rh/controllers/rh.controller.ts
+++ src/user/rh/controllers/rh.controller.ts
@@ -1,28 +1,77 @@
-import { ApiGet, ApiList } from 'src/common/decorators/api-crud.decorator';
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+    ApiCreate,
+    ApiDelete,
+    ApiGet,
+    ApiList,
+    ApiUpdate,
+} from 'src/common/decorators/api-crud.decorator';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { ApiAuth } from 'src/common/decorators/api-auth.decorator';
 import { RequireRH } from 'src/auth/decorators/roles.decorator';
-import { RHUserService } from '../services/rh.service';
+import { RHService } from '../services/rh.service';
 import { ApiTags } from '@nestjs/swagger';
 import { RHUserDTO } from '../dto/rh.dto';
+import { CreatePillarDto } from 'src/pillars/dto/create-pillar.dto';
+import { UpdatePillarDto } from 'src/pillars/dto/update-pillar.dto';
+import { CreateCriterionDto } from 'src/criteria/dto/create-criterion.dto';
+import { CreateCycleConfigDto } from 'src/cycle-config/dto/create-cycle-config.dto';
 
-@ApiTags('Usuários RH')
+@ApiTags('Admin RH')
 @ApiAuth()
-@Controller('rh/users')
-export class RHUserController {
-    constructor(private readonly rhUserService: RHUserService) {}
+@Controller('rh/')
+export class RHController {
+    constructor(private readonly rh: RHService) {}
 
+    // Usuário do tipo RH
     @RequireRH()
     @Get()
     @ApiList('usuários RH')
     async findAll(): Promise<RHUserDTO[]> {
-        return this.rhUserService.findAll();
+        return this.rh.findAll();
     }
 
     @RequireRH()
     @Get(':id')
     @ApiGet('usuário RH')
     async findOne(@Param('id', ParseIntPipe) id: number): Promise<RHUserDTO> {
-        return this.rhUserService.findOne(id);
+        return this.rh.findOne(id);
+    }
+
+    // Pilares
+    @RequireRH()
+    @Post('pillars')
+    @ApiCreate('Pilar')
+    createPillar(@Body() dto: CreatePillarDto) {
+        return this.rh.createPillar(dto);
+    }
+
+    @RequireRH()
+    @Put('pillars/:id')
+    @ApiUpdate('Pillar')
+    updatePillar(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdatePillarDto) {
+        return this.rh.updatePillar(id, dto);
+    }
+
+    @RequireRH()
+    @Delete('pillars/:id')
+    @ApiDelete('Pilar')
+    deletePillar(@Param('id', ParseIntPipe) id: number) {
+        return this.rh.deletePillar(id);
+    }
+
+    // Critérios
+    @RequireRH()
+    @Post('criteria')
+    @ApiCreate('Critério')
+    createCriterion(@Body() dto: CreateCriterionDto) {
+        return this.rh.createCriterion(dto);
+    }
+
+    // Ciclo
+    @RequireRH()
+    @Post('cycle')
+    @ApiCreate('Ciclo')
+    createCycle(@Body() dto: CreateCycleConfigDto) {
+        return this.rh.createCycle(dto);
     }
 }
--- src/user/rh/rh.module.ts
+++ src/user/rh/rh.module.ts
@@ -3,13 +3,13 @@ import { RhPanelController } from './controllers/rh-panel.controller';
 import { RhPanelService } from './services/rh-panel.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { CommonModule } from '../../common/common.module';
-import { RHUserController } from './controllers/rh.controller';
-import { RHUserService } from './services/rh.service';
+import { RHController } from './controllers/rh.controller';
+import { RHService } from './services/rh.service';
 
 @Module({
     imports: [PrismaModule, CommonModule],
-    controllers: [RhPanelController, RHUserController],
-    providers: [RhPanelService, RHUserService],
-    exports: [RhPanelService, RHUserService],
+    controllers: [RhPanelController, RHController],
+    providers: [RhPanelService, RHService],
+    exports: [RhPanelService, RHService],
 })
 export class RhModule {}
