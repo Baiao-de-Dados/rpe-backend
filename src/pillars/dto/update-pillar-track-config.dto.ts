ec07193263b5d416c9133e1980434f011b01af4f
n3waz <lhk@cesar.school>
2025-07-06 06:44:09 -0300

    fix: realloc pillars and deleted tags

--- src/pillars/dto/update-pillar-track-config.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePillarTrackConfigDto } from './create-pillar-track-config.dto';
-
-export class UpdatePillarTrackConfigDto extends PartialType(CreatePillarTrackConfigDto) {}
--- src/pillars/dto/update-pillar.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePillarDto } from './create-pillar.dto';
-
-export class UpdatePillarDto extends PartialType(CreatePillarDto) {}
