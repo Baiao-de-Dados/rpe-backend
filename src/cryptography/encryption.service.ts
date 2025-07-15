6257ccadd102436e77b75fb46f86408a42c54aad
n3waz <lhk@cesar.school>
2025-07-15 04:11:17 -0300

    feat: encryption service now have salt and pepper methods when crypt

--- src/cryptography/encryption.service.ts
+++ src/cryptography/encryption.service.ts
@@ -3,29 +3,38 @@ import * as crypto from 'crypto';
 
 const ALGORITHM = 'aes-256-cbc';
 const IV_LENGTH = 16;
-
-const SECRET = process.env.ENCRYPTION_KEY;
-if (!SECRET) {
-    throw new Error('ENCRYPTION_KEY environment variable is not set');
-}
-const KEY = crypto.createHash('sha256').update(SECRET).digest(); // adicionar sal e pepper -> hash(salt + bcrypt + pepper)
+const KEY_LENGTH = 32;
+const ITERATIONS = 10_000;
+const DIGEST = 'sha512';
+const SECRET = getRequiredEnv('ENCRYPTION_KEY');
+const PEPPER = getRequiredEnv('ENCRYPTION_PEPPER');
 
 @Injectable()
 export class EncryptionService {
-    encrypt(text: string): string {
+    encrypt(plainText: string): string {
+        const salt = crypto.randomBytes(16).toString('hex');
+        const key = crypto.pbkdf2Sync(SECRET + PEPPER, salt, ITERATIONS, KEY_LENGTH, DIGEST);
         const iv = crypto.randomBytes(IV_LENGTH);
-        const cipher = crypto.createCipheriv(ALGORITHM, KEY, iv);
-        let encrypted = cipher.update(text, 'utf8', 'hex');
-        encrypted += cipher.final('hex');
-        return iv.toString('hex') + ':' + encrypted;
+
+        const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
+        const ciphertext = cipher.update(plainText, 'utf-8', 'hex') + cipher.final('hex');
+        return `${salt}:${iv.toString('hex')}:${ciphertext}`;
     }
 
-    decrypt(data: string): string {
-        const [ivHex, encrypted] = data.split(':');
+    decrypt(encrypted: string): string {
+        const [salt, ivHex, ciphertext] = encrypted.split(':');
+        const key = crypto.pbkdf2Sync(SECRET + PEPPER, salt, ITERATIONS, KEY_LENGTH, DIGEST);
         const iv = Buffer.from(ivHex, 'hex');
-        const decipher = crypto.createDecipheriv(ALGORITHM, KEY, iv);
-        let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-        decrypted += decipher.final('utf8');
-        return decrypted;
+        const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
+        const plainText = decipher.update(ciphertext, 'hex', 'utf-8') + decipher.final('utf-8');
+        return plainText;
     }
+}
+
+function getRequiredEnv(key: string): string {
+    const val = process.env[key];
+    if (!val) {
+        throw new Error(`Variável de ambiente ${key} não está definida`);
+    }
+    return val;
 }
