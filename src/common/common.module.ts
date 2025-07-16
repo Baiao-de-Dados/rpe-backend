7bdf8eae8c3415798df02045eca86be23292544d
Paulo Rosado <paulohenrique110803@gmail.com>
2025-07-16 06:34:42 -0300

    fix: fixing common.module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { CryptoModule } from '../cryptography/crypto.module';
 import { SystemConfigService } from './services/system-config.service';
 import { SeedService } from './services/seed.service';
 import { SeedController } from './controllers/seed.controller';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, CryptoModule],
     providers: [SystemConfigService, SeedService],
     controllers: [SeedController],
     exports: [SystemConfigService],
